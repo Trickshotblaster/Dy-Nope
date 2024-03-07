1eaf59973fff644369fbbfb805b39bcde19a52ff
brogrammer <107130695+Trickshotblaster@users.noreply.github.com>
2024-03-07 14:09:36 -0800

    Update stopper.py
    Removed unnecessary imports, added some documentation comments

--- stopper.py
+++ stopper.py
@@ -1,9 +1,9 @@
-import os
-import signal
+# import necessary libraries
 import psutil
-import subprocess
 import wmi
+# initialize WMI
 f = wmi.WMI()
+# function to end a process with given id
 def terminate_process_id(process_id):
     for process in f.Win32_Process (ProcessId=process_id):
         print(process.ProcessId, process.Name)
@@ -12,14 +12,16 @@ def terminate_process_id(process_id):
         print("Process terminated successfully: %d" % process.ProcessId)
     except:
         print("There is no process running with id: %d" % process_id)
-
+# constantly loop through processes
 while True:
     for p in psutil.process_iter(attrs=["pid", "name"]):
+        # try to get the exe file location of each process
         path = ""
         try:
             path = str(p.exe())
         except:
             print("Couldn't fetch path")
+        # if file path contains "DyKnow" then kill the process
         if path.find("DyKnow") != -1:
             print("Dyknow found", p.info['pid'], path)
             terminate_process_id(p.info['pid'])
