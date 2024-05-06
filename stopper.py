68bb185faea64092dd5efd3730c0e72adb9425c5
brogrammer <107130695+Trickshotblaster@users.noreply.github.com>
2024-05-06 09:07:44 -0700

    Update stopper.py
    trying to optimize it

--- stopper.py
+++ stopper.py
@@ -1,27 +1,20 @@
-# import necessary libraries
 import psutil
 import wmi
-# initialize WMI
+
 f = wmi.WMI()
-# function to end a process with given id
-def terminate_process_id(process_id):
-    for process in f.Win32_Process (ProcessId=process_id):
-        print(process.ProcessId, process.Name)
-    try:
-        process.Terminate()
-        print("Process terminated successfully: %d" % process.ProcessId)
-    except:
-        print("There is no process running with id: %d" % process_id)
-# constantly loop through processes
-while True:
+
+def terminate_dyknow_processes():
     for p in psutil.process_iter(attrs=["pid", "name"]):
-        # try to get the exe file location of each process
-        path = ""
-        try:
-            path = str(p.exe())
-        except:
-            print("Couldn't fetch path")
-        # if file path contains "DyKnow" then kill the process
-        if path.find("DyKnow") != -1:
-            print("Dyknow found", p.info['pid'], path)
-            terminate_process_id(p.info['pid'])
+        if p.pid != 0:
+            try:
+                path = p.exe()
+                if "DyKnow" in path:
+                    p.terminate()
+                    print("DyKnow process terminated successfully: %d" % p.pid)
+            except psutil.NoSuchProcess:
+                pass
+            except psutil.AccessDenied:
+                print("Access denied to terminate process: %d" % p.pid)
+
+while True:
+    terminate_dyknow_processes()
